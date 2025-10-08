786adac2b1433deda3c34269c13bbd5983b8076c
김시원 <163103752+Huro0906@users.noreply.github.com>
2025-10-08 20:15:40 +0900

    [Gold V] Title: 토마토, Time: 616 ms, Memory: 150964 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> q = new LinkedList<>();
+        Integer zeroC =0 ;
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+
+            for(int j=0; j<m; j++){
+                int tmp = Integer.parseInt(st.nextToken());
+                arr[i][j] = tmp;
+                if(tmp==1){
+                    q.add(new int[]{j,i});
+                }else if(tmp ==0){
+                    zeroC++;
+                }
+            }
+        }
+        System.out.println(bfs(arr,q,n,m,visited,zeroC));
+        
+        
+    }
+
+    public static int bfs(int[][] arr, Queue<int[]> q, int n,int m,boolean[][] visited,Integer zeroC){
+        
+        int day = 0;
+        int[] dx = new int[]{1,0,-1,0};
+        int[] dy = new int[]{0,1,0,-1};
+        
+        while(!q.isEmpty()){
+            Queue<int[]> newQ = new LinkedList<>();
+            if(zeroC==0)break;
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int[] current = q.poll();
+                int curX =  current[0];
+                int curY = current[1];
+
+                for(int j=0; j<4; j++){
+                    int nx = curX+dx[j];
+                    int ny = curY+dy[j];
+                    if(nx>=0 && nx<m && ny >=0 && ny <n && arr[ny][nx]==0 && !visited[ny][nx]){
+                        arr[ny][nx] = 1;
+                        visited[ny][nx]= true;
+                        newQ.add(new int[]{nx,ny});
+                        zeroC--;
+                    }
+                }
+            }
+            q = newQ;
+            day++;
+        }
+
+        if(zeroC==0){
+            return day;
+        }
+        else{
+            return -1;
+        }
+        
+    }   
+}
